7ccbdd4c3f57799d19d3abe81f2493830d72cc4e
MedtheVorg <68510473+MedtheVorg@users.noreply.github.com>
2021-10-21 00:16:48 +0100

    tried another approach

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
 const form_submitbtn = document.querySelector(".form__submit");
-const form__inputs = document.querySelectorAll(".form__inputwarning");
+const firstname = document.querySelector("#first-name");
+const lastname = document.querySelector("#last-name");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
 
-form_submitbtn.addEventListener("click", function () {
-  console.log(this.className, form__inputs.length);
-  for (let i = 0; i < form__inputs.length; i++) {
-    console.log(form__inputs[i]);
-    if (form__inputs[i].querySelector("input").value == "") {
-      form__inputs[i].querySelector("input").style.border = "2px solid #ff7a7a";
-      form__inputs[i].querySelector(".warning").classList.remove("hidden");
-      form__inputs[i].querySelector(".message").classList.remove("hidden");
-    } else {
-      form__inputs[i].querySelector("input").style.border =
-        "1px solid rgba(0, 0, 0, 0.1)";
-      form__inputs[i].querySelector(".warning").classList.add("hidden");
-      form__inputs[i].querySelector(".message").classList.add("hidden");
-    }
+const checkFN = function () {
+  if (firstname.value == "") {
+    firstname.parentElement.classList.add("text-field--error");
+    firstname.parentElement.querySelector(".text-field__error").innerHTML =
+      "First name cannot be Empty";
+  } else {
+    firstname.parentElement.classList.remove("text-field--error");
+    firstname.parentElement.querySelector(".text-field__error").innerHTML = "";
   }
+};
+const checkLN = function () {
+  if (lastname.value == "") {
+    lastname.parentElement.classList.add("text-field--error");
+    lastname.parentElement.querySelector(".text-field__error").innerHTML =
+      "Last name cannot be Empty";
+  } else {
+    lastname.parentElement.classList.remove("text-field--error");
+    lastname.parentElement.querySelector(".text-field__error").innerHTML = "";
+  }
+};
+const checkemail = function () {
+  if (email.value == "" || email.value.indexOf("@") == -1) {
+    email.parentElement.classList.add("text-field--error");
+    email.parentElement.querySelector(".text-field__error").innerHTML =
+      "Looks like this is not an email";
+  } else {
+    email.parentElement.classList.remove("text-field--error");
+    email.parentElement.querySelector(".text-field__error").innerHTML = "";
+  }
+};
+const checkpassword = function () {
+  if (password.value == "") {
+    password.parentElement.classList.add("text-field--error");
+    password.parentElement.querySelector(".text-field__error").innerHTML =
+      "Password cannot be Empty";
+  } else {
+    password.parentElement.classList.remove("text-field--error");
+    password.parentElement.querySelector(".text-field__error").innerHTML = "";
+  }
+};
+
+firstname.addEventListener("input", checkFN);
+firstname.addEventListener("blur", checkFN);
+lastname.addEventListener("input", checkLN);
+lastname.addEventListener("blur", checkLN);
+email.addEventListener("input", checkemail);
+email.addEventListener("blur", checkemail);
+password.addEventListener("input", checkpassword);
+password.addEventListener("blur", checkpassword);
+
+///Form SUBMIT BTN
+form_submitbtn.addEventListener("click", () => {
+  checkFN();
+  checkLN();
+  checkemail();
+  checkpassword();
 });
